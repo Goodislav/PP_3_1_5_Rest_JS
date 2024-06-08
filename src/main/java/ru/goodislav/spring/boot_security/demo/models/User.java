77a59549d290286267e2db24db1c0126bb05f071
Yury Goodyma <goodislav@gmail.com>
2024-06-08 14:30:18 +0300

    add classes and dependencies

---
+++ src/main/java/ru/goodislav/spring/boot_security/demo/models/User.java
@@ -0,0 +1,103 @@
+package ru.goodislav.spring.boot_security.demo.models;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+    @Column(name = "lastname")
+    @NotEmpty(message = "Lastname should not be empty")
+    @Size(min = 2, max = 70, message = "Lastname should be between 2 and 70 characters")
+    private String lastname;
+    @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Max(value = 120, message = "Age should be less than 120")
+    private int age;
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+    @Column(name = "password")
+    @NotEmpty(message = "Password should not be empty")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$",
+            message = "Password should be between 8 and 16 characters and contain " +
+                    "at least one uppercase letter, one lowercase letter, one digit, " +
+                    "with no spaces and one special character")
+    private String password;
+
+    public User() {
+    }
+    public User(String name, String lastname, int age, String email, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
